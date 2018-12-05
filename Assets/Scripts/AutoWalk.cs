1f8c0c9f6dda95d7c46c14efe1fcb4fc84a0e196
Adalberto Adolfo Adam <testes.atenas@gmail.com>
2018-12-04 23:22:53 -0200

    Ajuste MovimentaÃO

---
+++ Assets/Scripts/AutoWalk.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoWalk : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform player;
+	public float speed;
+	private bool isWaling;
+	private const int MAX_ANGLE = 90;
+	public float threshold;
+	public bool freezeY;
+
+	
+	// Update is called once per frame
+	void Update () {
+		isWaling = !isWaling && 
+		transform.eulerAngles.x >= threshold && 
+		transform.eulerAngles.x <= MAX_ANGLE;	
+
+		if(isWaling){
+			Vector3 direcao = transform.TransformDirection(Vector3.forward);
+			player.Translate(direcao* speed);
+		}
+		if(freezeY){
+			player.transform.position = new Vector3(
+				player.transform.position.x,
+				1,
+				player.transform.position.z
+			);
+		}
+	}
+	public void JumpToPosition(Transform wayPoint){
+		player.transform.position = wayPoint.position;
+	}
+}
---
+++ Assets/Scripts/CasaControllers.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CasaControllers : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject porta;
+	public bool abrePorta=false;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+	public void SelectObjects(Transform wayPoint){
+		if (wayPoint.gameObject.name == "car_key"){
+			Destroy(wayPoint.gameObject);
+			porta.GetComponent<Collider>().enabled=true;
+		}
+		if (wayPoint.gameObject.name == "Porta"){
+			if (abrePorta){
+				SceneManager.LoadScene("Cidade");
+			}
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("GameController")){
+			abrePorta=true;
+		}else{
+			abrePorta=false;	
+		}
+	}
+}
